dd956a4c19114ac888b2307396780aa0ac6cdb01
jbergknoff <jonathan@bergknoff.com>
2015-04-28 10:30:59 -0500

    z

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -7,7 +7,7 @@ if [ -n $ROOT_PASSWORD ]; then
   echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD'" >> $INITFILE
 fi
 
-if [ -n "$USERNAME" ] then
+if [ -n "$USERNAME" ]; then
   echo "GRANT ALL ON *.* TO '$USERNAME'@'%' IDENTIFIED BY '$PASSWORD'" >> $INITFILE
 fi
 
